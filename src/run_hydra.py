b3bd50a14ec2fd526d17dfbb81eb87aa0f1ebfcd
Denis Turcu <denis.turcu.11@gmail.com>
2025-08-13 20:53:44 -0700

    comment backend line (warning) and "gpu" line (raising an error when no gpu is available.

--- src/run_hydra.py
+++ src/run_hydra.py
@@ -1,11 +1,14 @@
-import os 
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"  # Use GPU 1
+import os
+
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0"  # Use GPU 1
 os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = "0.95"
 import jax
 
 # Configure JAX for better performance
 jax.config.update("jax_enable_x64", False)  # Use float32 for better GPU performance
-print('Gpu devices:', len(jax.devices('gpu')) if jax.devices('gpu') else len(jax.devices('cpu')))
+
+# print("JAX backend:", jax.lib.xla_bridge.get_backend().platform)
+# print('Gpu devices:', len(jax.devices('gpu')) if jax.devices('gpu') else len(jax.devices('cpu')))
 
 import hydra
 import sparse
@@ -14,6 +17,7 @@ from pathlib import Path
 import src.io_dict_to_hdf5 as ioh5
 from src.path_utils import convert_dict_to_path, save_config
 from src.vnc_sim import run_vnc_simulation
+
 
 @hydra.main(version_base=None, config_path="../configs", config_name="config")
 def main(cfg: DictConfig):
@@ -31,13 +35,15 @@ def main(cfg: DictConfig):
     ##### Run the simulation #####
     print("Running VNC simulation with the following configuration:")
     results, final_mini_circuits, neuron_params = run_vnc_simulation(cfg)
-    
+
     ##### Save results #####
-    print('Saving results to:', cfg.paths.ckpt_dir)
+    print("Saving results to:", cfg.paths.ckpt_dir)
     sparse.save_npz(cfg.paths.ckpt_dir / f"{cfg.experiment.name}_Rs.npz", sparse.COO.from_numpy(results))
-    ioh5.save(cfg.paths.ckpt_dir / 'neuron_params.h5', neuron_params._asdict())
+    ioh5.save(cfg.paths.ckpt_dir / "neuron_params.h5", neuron_params._asdict())
     if final_mini_circuits is not None:
-        sparse.save_npz(cfg.paths.ckpt_dir / f"{cfg.experiment.name}_mini_circuits.npz", sparse.COO.from_numpy(final_mini_circuits))
+        sparse.save_npz(
+            cfg.paths.ckpt_dir / f"{cfg.experiment.name}_mini_circuits.npz", sparse.COO.from_numpy(final_mini_circuits)
+        )
 
 
 if __name__ == "__main__":
